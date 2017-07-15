d4a0e66518e6201a674a3f85e9139318729c04da
Ekaterina Fomenko <katfom@gmail.com>
2017-07-15 20:00:39 +0300

    Add VK const usage

--- src/main/java/com/editor/TextInputMap.java
+++ src/main/java/com/editor/TextInputMap.java
@@ -1,17 +1,18 @@
 package main.java.com.editor;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 
 public class TextInputMap extends InputMap{
     {
         for(char i = ' '; i <='~';i++ ){
             put(KeyStroke.getKeyStroke(i), Character.toString(i));
         }
-        put(KeyStroke.getKeyStroke("BACK_SPACE"), "delete");
-        put(KeyStroke.getKeyStroke("ENTER"), "newLine");
-        put(KeyStroke.getKeyStroke("RIGHT"),"right");
-        put(KeyStroke.getKeyStroke("LEFT"),"left");
-        put(KeyStroke.getKeyStroke("UP"),"up");
-        put(KeyStroke.getKeyStroke("DOWN"),"down");
+        put(KeyStroke.getKeyStroke(KeyEvent.VK_BACK_SPACE, 0), "delete");
+        put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "newLine");
+        put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0),"right");
+        put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0),"left");
+        put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0),"up");
+        put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0),"down");
     }
 }
