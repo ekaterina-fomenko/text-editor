7f005c68956af28b2f2048e1b0ab2cc6273964ba
Ekaterina Fomenko <katfom@gmail.com>
2017-07-18 01:14:26 +0300

    Add reserved words for haskell and erlang

--- src/main/java/com/editor/parser/Regexp.java
+++ src/main/java/com/editor/parser/Regexp.java
@@ -1,10 +1,14 @@
 package com.editor.parser;
+
 /**
- * This class includes all regex templates
+ * This class includes all regexp templates
  */
 public class Regexp {
     public static final String BEFORE_REGEX = "(?<=^|\\s|\\(|\\{|\\[|\\)|\\}|\\]|,|\\+|\\-|\\*|\\/)";
     public static final String AFTER_REGEX = "(?=$|\\s|\\(|\\{|\\[|\\)|\\}|\\]|,|\\+|\\-|\\*|\\/)";
 
     public static final String JS_RESERVED_WORDS = "(var|new|class|this|super|implements|extends|interface|import|function|public|default|private|protected|abstract|static|final|finally|void|null|int|double|long|byte|native|char|float|short|enum|synchronized|true|false|volatile|goto|while|throws|boolean|export|in|try|catch|delete|throw|with|while|for|continue|return|else|typOf|instanceOf|break|do|if|package|transient)";
+    public static final String HASKELL_RESERVED_WORDS = "(case|class|data|deriving|do|else|if|import|in|infix|infixl|infixr|instance|let|of|module|newtype|then|type|where|foreign|forall|hiding|proc|rec|qualified)";
+    public static final String ERLANG_RESERVED_WORDS = "(after|case|or|and|orelse|analso|cond|query|band|div|receive|rem|end|begin|bnot|bor|if|let|xor|bsl|bsr|not|of|bxor|try|when|ok|error|undefined|reply|noreply|stop|ignore)";
+
 }
