b79daefde2c9a3ce87b8b134e85f5976cfdb0d39
Ekaterina Fomenko <katfom@gmail.com>
2017-07-20 21:11:55 +0300

    Add reserved words for haskell

--- src/main/java/com/editor/parser/Regexp.java
+++ src/main/java/com/editor/parser/Regexp.java
@@ -9,7 +9,7 @@ public class Regexp {
 
     public static final String PLAIN_TEXT_WORDS = "";
     public static final String JS_RESERVED_WORDS = "(var|new|class|this|super|implements|extends|interface|import|function|public|default|private|protected|abstract|static|final|finally|void|null|int|double|long|byte|native|char|float|short|enum|synchronized|true|false|volatile|goto|while|throws|boolean|export|in|try|catch|delete|throw|with|while|for|continue|return|else|typOf|instanceOf|break|do|if|package|transient)";
-    public static final String HASKELL_RESERVED_WORDS = "(case|class|data|deriving|do|else|if|import|in|infix|infixl|infixr|instance|let|of|module|newtype|then|type|where|foreign|forall|hiding|proc|rec|qualified)";
+    public static final String HASKELL_RESERVED_WORDS = "(return|case|class|data|deriving|do|else|if|import|in|infix|infixl|infixr|instance|let|of|module|newtype|then|type|where|foreign|forall|hiding|proc|rec|qualified)";
     public static final String ERLANG_RESERVED_WORDS = "(fun|module|export|import|after|case|or|and|orelse|andalso|cond|query|band|div|receive|rem|end|begin|bnot|bor|if|let|xor|bsl|bsr|not|of|bxor|try|when|ok|error|undefined|reply|noreply|stop|ignore)";
 
 }
