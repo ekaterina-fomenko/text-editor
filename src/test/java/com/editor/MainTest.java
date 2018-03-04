99c66238fe411b82231d4718e3b65e66abdeeb83
Ekaterina Fomenko <katfom@gmail.com>
2018-03-04 22:45:44 +0300

    Add test on filepath passing via cli

---
+++ src/test/java/com/editor/MainTest.java
@@ -0,0 +1,12 @@
+package com.editor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void tryFetFileArg() throws Exception {
+        assertEquals("filePath", Main.tryFetFileArg(new String[]{"--file:filePath"}));
+    }
+}
